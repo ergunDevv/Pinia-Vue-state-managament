dd6c7651ce045005fc735deb7fce174f59b7cc2e
ergunDevv <ergncyln2127088@gmail.com>
2023-02-19 00:25:40 +0300

    Filtering the todos according to the filter button

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@ import { storeToRefs } from 'pinia';
 // import counterComponent from './components/counterComponent.vue'
 
 const todosStore = useTodosStore();
-const {todos} = storeToRefs(todosStore);
-const {addTodo} = todosStore;
+const {filteredTodos} = storeToRefs(todosStore);
+const {addTodo,toggleTodo,changeFilter} = todosStore;
 const valueInput =ref("");
 
 //Destructiring the pinia addTodo function
@@ -21,12 +21,17 @@ valueInput.value=""
 
 <template>
   <main>
+    <div>
+      <button @click="changeFilter('all')">all</button>
+      <button @click="changeFilter('finished')">finished</button>
+      <button @click="changeFilter('unfinished')">unfinished</button>
+    </div>
     <input type="text" v-model="valueInput" />
     <button @click="()=>addNewTodo(valueInput)">Add</button>
 
-    <div v-for="todo in todos" :key="todo.id" style="display: flex">
-      <h1>{{ todo.text }}</h1>
-      <button>Todo is Done</button>
+    <div v-for="todo in filteredTodos" :key="todo.id" style="display: flex">
+      <h1 :style="todo.isFinished && {textDecoration:'line-through'} ">{{ todo.text }}</h1>
+      <button @click="toggleTodo(todo.id)">{{todo.isFinished}} = Todo is Done</button>
     </div>
     <!-- <counterComponent/> -->
   </main>
--- src/store/todos.js
+++ src/store/todos.js
@@ -12,7 +12,19 @@ export const useTodosStore = defineStore("todos", {
         isFinished: false,
       },
     ],
+    filter:"unfinished",
+
   }),
+  getters:{
+    filteredTodos(){
+        if(this.filter === "finished"){
+            return this.todos.filter(todo=>todo.isFinished)
+        }else if(this.filter==="unfinished"){
+            return this.todos.filter(todo=>!todo.isFinished)            
+        }
+        return this.todos;
+    }
+  },
   actions : {
     addTodo(text){
         if(!text) return;
@@ -21,6 +33,13 @@ export const useTodosStore = defineStore("todos", {
             text,
             isFinished:false,
         })
+    },
+    toggleTodo(id){
+        const index = this.todos.findIndex((todo)=>todo.id===id);
+        this.todos[index].isFinished=!this.todos[index].isFinished;
+    },
+    changeFilter(filterText){
+        this.filter=filterText;
     }
   }
 });
