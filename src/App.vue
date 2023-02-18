f68dfa2784d3d670069e39351a157e1435575a15
ergunDevv <ergncyln2127088@gmail.com>
2023-02-18 23:08:03 +0300

    Initial project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+const todos = [
+  {
+    id:1,
+    text:"clean room",
+    isFinished:false,
+  }];
+</script>
+
+
+<template>
+  <main>
+    <input type="text" />
+    <button>Add</button>
+    <h1>{{todos[0].text}}</h1>
+  </main>
+</template>
+  
